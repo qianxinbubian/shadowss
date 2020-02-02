6523bfd1d30a166423743c0bdf8a09ba66644cde
qiaoxiaoqianxi <liminghua13118@163.com>
2020-02-02 10:54:22 +0800

    change address

--- url.py
+++ url.py
@@ -6,5 +6,5 @@
 #         Date            :   2020年01月27日 星期一 21时28分15秒
 #==================================================================================================
 
-Inf =[['apps','\'https://github-production-release-asset-2e65be.s3.amazonaws.com/51980455/b11d9300-32bb-11ea-91be-6e5f90b58d32?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20200131%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20200131T065423Z&X-Amz-Expires=300&X-Amz-Signature=6db196758fa30ed7d90f01560ddccaf1705436b4bc40c977bc79b92ec3c997e5&X-Amz-SignedHeaders=host&actor_id=34475861&response-content-disposition=attachment%3B%20filename%3DAlacritty-v0.4.1-windows-portable.zip&response-content-type=application%2Foctet-stream\'']]
+Inf =[['archwsl','https://github.com/alacritty/alacritty/releases/download/v0.4.1/Alacritty-v0.4.1-windows-portable.zip']]
 # print(Inf[0][1])
